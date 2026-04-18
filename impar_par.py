7bfa0a20cb6d91e07b4bc75ea9e8d03b233ff236
AlvaroM_araujo <alvaromarqurs@gmail.com>
2026-04-17 21:49:36 -0300

    Add even or odd number checker in impar_par.py
    Implement a program to check if a number is even or odd.

---
+++ impar_par.py
@@ -0,0 +1,21 @@
+#descobrir se um numero é par ou impar
+numero = int(input('Digite um numero: '))
+if numero %2==0:
+    print("o numero é par")
+else:
+    print('o numero é impar')
+
+#  operadores logicos 
+
+# +  -> Soma (prioridade 4)
+# -  -> Subtração (prioridade 4)
+
+# *  -> Multiplicação (prioridade 3)
+# /  -> Divisão (prioridade 3)
+# // -> Divisão inteira (retorna só a parte inteira) (prioridade 3)
+# %  -> Resto da divisão (módulo) (prioridade 3)
+
+# ** -> Exponenciação (potência) (prioridade 2)
+
+# + (unário) -> Mantém o sinal do número (prioridade 1)
+# - (unário) -> Inverte o sinal do número (prioridade 1)
